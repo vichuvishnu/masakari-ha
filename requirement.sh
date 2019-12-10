ffd70686582b55ac9998561ca1761aa4966e3e75
Vishnu Bhaskar <41546654+vichuvishnu@users.noreply.github.com>
2019-12-10 17:30:17 +0530

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -3,3 +3,9 @@ sudo apt-get install libpq-dev -y
 sudo apt-get install python-dev -y
 sudo apt-get install gcc -y
 sudo apt-get install python3-dev -y
+sudo pip install oslo_policy
+sudo pip install oslo_db
+sudo pip install sqlalchemy
+sudo pip install flask_sqlalchemy
+sudo pip install Flask-SQLAlchemy
+sudo pip install oslo.service
