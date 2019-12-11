d6a244bb5384f87bc4cdb2eec0936f334ccef800
Vishnu Bhaskar <41546654+vichuvishnu@users.noreply.github.com>
2019-12-11 16:14:46 +0530

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -1,31 +1,55 @@
 #!/bin/bash
-apt-packages=(python-pip python3-pip libvirt-dev python-dev python3-dev gcc make)
-pip-packages=(tox oslo_policy oslo_db sqlalchemy flask_sqlalchemy Flask-SQLAlchemy oslo.service)
-sizeOfAptPack=${#apt-packages[@]}
-sizeOfPipPack=${#pip-packages[@]}
+rd=`tput setaf  1`
+gr=`tput setaf  2`
+yl=`tput setaf  3`
+rs=`tput sgr0`
+COLUMNS=`tput cols`
+aptPackages=(python-pip python3-pip libvirt-dev python-dev python3-dev gcc make)
+pipPackages=(tox oslo_policy oslo_db sqlalchemy flask_sqlalchemy Flask-SQLAlchemy oslo.service)
+sizeOfAptPack=${#aptPackages[@]}
+sizeOfPipPack=${#pipPackages[@]}
+function drawSummary {
+  size=$(((COLUMNS/2)-4))
+  printf "%*s" $size| tr " " "_"
+  printf " summary "
+  printf "%*s" $size| tr " " "_"
+}
+function die {
+   drawSummary
+   echo "${rd} $1 package installation failed"
+   echo "Installation exist :-(${rs}"
+   exit 0
+}
 function apt-install {
   local n=0
   while [ $n -lt $sizeOfAptPack ];do
-    echo "+-- sudo apt-get install ${apt-packages[$n]} -y"
-    sudo apt-get install ${sizeOfAptPack[$n]} -y
+    echo "${yl}+-- Installing ${aptPackages[$n]}${rs}"
+    sudo apt-get install ${aptPackages[$n]} -y ; local stat=$?
+    if [ $stat -ne 0 ];then
+       die ${sizeOfAptPack[$n]}
+    fi
     n=` expr $n + 1 `
   done
 }
 function pip-install {
   local n=0
   while [ $n -lt $sizeOfPipPack ];do
-    echo "+-- sudo pip install ${pip-packages[$n]}"
-    sudo pip install ${pip-packages[$n]}
+    echo "${yl}+-- Installing ${pipPackages[$n]}${rs}"
+    sudo pip install ${pipPackages[$n]} ; local stat=$?
+    if [ $stat -ne 0 ];then
+       die ${sizeOfAptPack[$n]}
+    fi
     n=` expr $n + 1 `
   done
 }
 function installed-packages {
-  echo "${apt-packages}"
-  echo "${pip-packages}"
+  drawSummary
+  echo "${gr} ./requirement.sh"
+  echo "Required Package Installation success :-)${rs}"
+  echo "$(tput bold)${aptPackages[@]}"
+  echo "${pipPackages[@]}$(tput sgr0)"
 }
-
+#main
 apt-install
-
 pip-install
-
 installed-packages
